81128bcf531250b32d30e1c11785e689df167a0e
MIKE ANTHONY ECHON (SHP-IRIS-ISD-OOCLL/MNL) <ECHONMI@OOCL.COM>
2019-10-21 12:39:03 +0800

    complete ac4

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -2,24 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const clickFxn = () =>{
-  alert("now you know how to handle onclick event")
-}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick= {clickFxn}>Hello World
-        </button>
-        <p>number : 0</p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        number: 0
+      };
+  }
+  
+  render(){
+    const buttonClk = () => {
+      this.setState({number: 1});
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={buttonClk}>Hello World
+          </button>
+          <p>{this.state.number}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
